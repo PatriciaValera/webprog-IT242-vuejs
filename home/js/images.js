652c20edd452ce0970ac386fe5fc3944b2cd3386
Patricia Marie Valera <pdvalera@student.apc.edu.ph>
2026-01-08 20:06:06 +0800

    r

--- home/js/images.js
+++ home/js/images.js
@@ -2,7 +2,7 @@ const app2 = Vue.createApp({
   data() {
     return {
       manyPictures: [
-        'https://cdn.shopify.com/s/files/1/0416/8083/0620/files/01132022_soc_pinterestboardcoverupdate_KU_1200x1200_1_1024x1024.png?v=1664470975',
+        'https://www.w3schools.com/vue/img_burrito.svg',
         'https://www.w3schools.com/vue/img_salad.svg',
         'https://www.w3schools.com/vue/img_cake.svg',
         'https://www.w3schools.com/vue/img_soup.svg',
