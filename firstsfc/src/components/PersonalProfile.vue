7e78eb1032db139439641cc5eeab35bc064034bd
Patricia Marie Valera <pdvalera@student.apc.edu.ph>
2026-01-16 11:17:14 +0800

    try5

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,22 +1,32 @@
-<template>
-<div class="container">
-    <header> <h1>Personal Profile Web page </h1>  </header>
-    <main>
-      <section class="main-content">
-        <div>1 About Me</div>
-        <div>2 Education / Achievements</div>
-        <div>3 Course </div>  
-        <div>4 IT Experience</div>
-        <div>5 Hobbies & Interests</div>
-        <div>6 Goals In Life / Dream</div>  
-        <div>7 Picture Gallery</div>
-      </section>
-      <aside class="sidebar">
-        </aside>
-    </main>
-    <footer> Asia Pacific College   </footer>
-  </div>
-</template>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tricia's Introduction</title>
+</head>
+<link rel="stylesheet" href="css/personalprofile.css">
+<body>
+<h1>Personal Profile Web page </h1>
+<p>
+About Me<br>
+Name: Patricia Marie D. Valera<br>
+Nickname: Tricia<br>
+ 
+Education / Achievements<br>
+Course: Bachelor of Science in Information Technology<br>
+IT Experience: Gaming, MySQL Workbench, OutSystems<br>
+Hobbies & Interests<br>
+  - Listening to music<br>
+  - Watching movies, animes, series<br>
+  - Reading novels, manhwa, wattpad<br>
+Goals In Life / Dream:<br>
+  - Learn how to Drive a big bike<br>
+  - Graduate on time<br>
+  - Visit different countries<br>
+Picture Gallery<br>
+<img src="https://i.pinimg.com/originals/3e/20/17/3e20170d75e4cbf183ee3c45759869d4.png" alt="kuromi" width="500" height="600">
+</p>
 
-<script></script>
+</body>
+</html>
+
 <style src="./css/personalprofile.css"></style>
