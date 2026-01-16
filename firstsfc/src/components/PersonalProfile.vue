87e5388a2e8f26e7875f9c5837b851ef57b8a1ac
Patricia Marie Valera <pdvalera@student.apc.edu.ph>
2026-01-16 11:40:29 +0800

    try7

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,48 +1,63 @@
-<template>
-<title>Tricia's Introduction</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tricia's Introduction</title>
+  <link rel="stylesheet" href="css/rwd.css">
 </head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</p>
+<body>
 
 <div class="grid-container">
 
-<div class="header"><h1>Tricia's Personal Profile Web page</h1></div>
+  <!-- Header -->
+  <div class="header">
+    <h1>Tricia's Personal Profile Web Page</h1>
+  </div>
 
-<div class="menu">
-  <ul>
-    <li>Plays badminton</li>
-    <li>Loves cycling</li>
-    <li>Obsessed with purple</li>
-    <li>Loves strolling</li>
-  </ul>
+  <!-- Menu -->
+  <div class="menu">
+    <ul>
+      <li>Plays badminton</li>
+      <li>Loves cycling</li>
+      <li>Obsessed with purple</li>
+      <li>Loves strolling</li>
+    </ul>
+  </div>
+
+  <!-- Content -->
+  <div class="content">
+    <h1>Tricia's Introduction</h1>
+    <p>
+      <strong>About Me</strong><br>
+      Name: Patricia Marie D. Valera<br>
+      Nickname: Tricia
+    </p>
+    <p>
+      <strong>Education / Achievements</strong><br>
+      Course: Bachelor of Science in Information Technology
+    </p>
+    <p>
+      <strong>Hobbies & Interests</strong><br>
+      - Listening to music<br>
+      - Watching movies, animes, series<br>
+      - Reading novels, manhwa, wattpad
+    </p>
+  </div>
+
+  <!-- Picture Gallery -->
+  <div class="pic">
+    <h2>Picture Gallery</h2>
+    <img src="https://i.pinimg.com/originals/aa/29/e9/aa29e9cae0a07ebf8444331813e8aba0.jpg" 
+         alt="Kuromi" width="250" height="250">
+  </div>
+
+  <!-- Footer -->
+  <div class="footer">
+    <p>The End.</p>
+  </div>
+
 </div>
 
-<div class="content">
-  <h1>Tricia's Introduction</h1>
-  <p>About Me<br>
-  Name: Patricia Marie D. Valera<br>
-  Nickname: Tricia<br></p>
-  <p>Education / Achievements<br>
-  Course: Bachelor of Science in Information Technology<br></p>
-  <p>Hobbies & Interests<br>
-  - Listening to music<br>
-  - Watching movies, animes, series<br>
-  - Reading novels, manhwa, wattpad<br></p>
-</div>  
-
-<div class="pic">
-  <h2>Picture Gallery</h2>
-  <ul>
-    <img src="https://i.pinimg.com/originals/aa/29/e9/aa29e9cae0a07ebf8444331813e8aba0.jpg" alt="kuromi" width="250" height="250">
-  </ul>
-</div>
-
-<div class="footer"><p>The End.</p></div>
-</div>
-
-</template>
-
-<script>
-</script>
-
-<style src="./css/PersonalProfile.css"></style>
+</body>
+</html>
