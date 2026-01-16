405ad1d61a13e039208cc79e37fd0a8b42e774c1
Patricia Marie Valera <pdvalera@student.apc.edu.ph>
2026-01-16 11:34:17 +0800

    try6

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,31 +1,48 @@
-<!DOCTYPE html>
-<html>
-<head>
+<template>
 <title>Tricia's Introduction</title>
 </head>
-<body>
-<h1>Personal Profile Web page </h1>
-<p>
-About Me<br>
-Name: Patricia Marie D. Valera<br>
-Nickname: Tricia<br>
- 
-Education / Achievements<br>
-Course: Bachelor of Science in Information Technology<br>
-IT Experience: Gaming, MySQL Workbench, OutSystems<br>
-Hobbies & Interests<br>
-  - Listening to music<br>
-  - Watching movies, animes, series<br>
-  - Reading novels, manhwa, wattpad<br>
-Goals In Life / Dream:<br>
-  - Learn how to Drive a big bike<br>
-  - Graduate on time<br>
-  - Visit different countries<br>
-Picture Gallery<br>
-<img src="https://i.pinimg.com/originals/3e/20/17/3e20170d75e4cbf183ee3c45759869d4.png" alt="kuromi" width="500" height="600">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </p>
 
-</body>
-</html>
+<div class="grid-container">
 
-<style src="./css/personalprofile.css"></style>
+<div class="header"><h1>Tricia's Personal Profile Web page</h1></div>
+
+<div class="menu">
+  <ul>
+    <li>Plays badminton</li>
+    <li>Loves cycling</li>
+    <li>Obsessed with purple</li>
+    <li>Loves strolling</li>
+  </ul>
+</div>
+
+<div class="content">
+  <h1>Tricia's Introduction</h1>
+  <p>About Me<br>
+  Name: Patricia Marie D. Valera<br>
+  Nickname: Tricia<br></p>
+  <p>Education / Achievements<br>
+  Course: Bachelor of Science in Information Technology<br></p>
+  <p>Hobbies & Interests<br>
+  - Listening to music<br>
+  - Watching movies, animes, series<br>
+  - Reading novels, manhwa, wattpad<br></p>
+</div>  
+
+<div class="pic">
+  <h2>Picture Gallery</h2>
+  <ul>
+    <img src="https://i.pinimg.com/originals/aa/29/e9/aa29e9cae0a07ebf8444331813e8aba0.jpg" alt="kuromi" width="250" height="250">
+  </ul>
+</div>
+
+<div class="footer"><p>The End.</p></div>
+</div>
+
+</template>
+
+<script>
+</script>
+
+<style src="./css/PersonalProfile.css"></style>
