dcddd05ec80f1880c3581c49df75a1026c3177fe
Patricia Marie Valera <pdvalera@student.apc.edu.ph>
2026-01-16 11:57:45 +0800

    try10

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,50 +1,55 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Tricia's Introduction</title>
-</head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/rwd.css">
-</p>
+<template>
+  <div>
+    <h1>Personal Profile Web Page</h1>
 
-<div class="grid-container">
+    <h2>About Me</h2>
+<div class="page-wrapper">
+    <div class="profile">
+      <div>Name: Sheila Nicole D. Cheng</div>
+      <div>
+        College undergraduate<br />
+        Course: BS Information Technology<br />
+        IT Experience: Can code with Python, JavaScript, HTML, CSS
+      </div>
+      <div>
+        Hobbies & Interests: Painting, Eating, Walking, Sleeping<br />
+        Goals In Life / Dream: I want to live my life to the fullest
+      </div>
+    </div>
 
-<div class="header"><h1>Tricia's Personal Profile Web page</h1></div>
+    <h2>Gallery</h2>
 
-<div class="menu">
-  <ul>
-    <li>Plays badminton</li>
-    <li>Loves cycling</li>
-    <li>Obsessed with purple</li>
-    <li>Loves strolling</li>
-  </ul>
+    <div class="container">
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTvfwpa0yWEj7tarq1Nfkk0NWRQ-uevnk8LYOd6w62F5JFyJ27xAL9su6bs_qE_4TVens&usqp=CAU"
+          width="200"
+          height="300"
+          alt="Gallery image 1"
+        />
+      </div>
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM4mQ0n9nyUvROIMh7Xa3SdZBAW1JBlWDd0MkaUvk7tiFi7QYDTw4iK8wW5GGLuz2AHXg&usqp=CAU"
+          width="200"
+          height="300"
+          alt="Gallery image 2"
+        />
+      </div>
+      <div>
+        <img
+          src="https://i.pinimg.com/236x/17/d7/ef/17d7efb9372de02f43f14e81b41d7a49.jpg"
+          width="200"
+          height="300"
+          alt="Gallery image 3"
+        />
+      </div>
+    </div>
+  </div>
 </div>
+</template>
 
-<div class="content">
-  <h1>Tricia's Introduction</h1>
-  <p>About Me<br>
-  Name: Patricia Marie D. Valera<br>
-  Nickname: Tricia<br></p>
-  <p>Education / Achievements<br>
-  Course: Bachelor of Science in Information Technology<br></p>
-  <p>Hobbies & Interests<br>
-  - Listening to music<br>
-  - Watching movies, animes, series<br>
-  - Reading novels, manhwa, wattpad<br></p>
-</div>  
-
-<div class="pic">
-  <h2>Picture Gallery</h2>
-  <ul>
-    <img src="https://i.pinimg.com/originals/aa/29/e9/aa29e9cae0a07ebf8444331813e8aba0.jpg" alt="kuromi" width="250" height="250">
-  </ul>
-</div>
-
-<div class="footer"><p>The End.</p></div>
-  
-</div>
-
-</body>
-</html>
+<script>
+</script>
 
 <style src="./css/personalprofile.css"></style>
