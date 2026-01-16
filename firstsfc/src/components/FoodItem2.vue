b66955ddb26b4b9fa419ba12eb45d26323f58cc8
Patricia Marie Valera <pdvalera@student.apc.edu.ph>
2026-01-16 10:17:49 +0800

    added fooditem2

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="avocado">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Mangoes and Avocados',
+      message: 'I like mangoes and avocados'
+    }
+  }
+};
+</script>
+
+<style>
+.avocado {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: rgb(204, 204, 255);
+}
+</style>
