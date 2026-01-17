581255aedd466712c95a25c0bc092d631ebeb851
Patricia Marie Valera <pdvalera@student.apc.edu.ph>
2026-01-17 21:13:43 +0800

    try04

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,57 +1,60 @@
 <template>
-  <div class="container">
-    <header>
+  <div class="grid-container">
+
+    <div class="header">
       <h1>Tricia's Personal Profile Web page</h1>
-    </header>
-    <main>
-      <section class="main-content">
-        <div id="div1">
-          <h2>About Me</h2>
-          <p>Name: Patricia Marie D. Valera<br>
-          Nickname: Tricia</p>
-        </div>
-        <div id="div2">
-          <h2>Education / Achievements</h2>
-          <p>Course: Bachelor of Science in Information Technology</p>
-        </div>
-        <div id="div3">
-          <h2>Course</h2>
-          <p>BS Information Technology at Asia Pacific College</p>
-        </div>
-        <div id="div4">
-          <h2>IT Experience</h2>
-          <p>Hands-on with HTML, CSS, Vue.js, and cloud deployment (Azure, AWS, Vercel)</p>
-        </div>
-        <div id="div5">
-          <h2>Hobbies & Interests</h2>
-          <ul>
-            <li>Listening to music</li>
-            <li>Watching movies, animes, series</li>
-            <li>Reading novels, manhwa, wattpad</li>
-            <li>Plays badminton</li>
-            <li>Loves cycling</li>
-            <li>Loves strolling</li>
-            <li>Obsessed with purple</li>
-          </ul>
-        </div>
-        <div id="div6">
-          <h2>Goals In Life / Dream</h2>
-          <p>To showcase my identity and skills through personal projects and grow as a developer.</p>
-        </div>
-        <div id="div7">
-          <h2>Picture Gallery</h2>
-          <img src="https://i.pinimg.com/originals/aa/29/e9/aa29e9cae0a07ebf8444331813e8aba0.jpg" alt="kuromi" width="250" height="250">
-        </div>
-      </section>
-      <aside class="sidebar">
-        <!-- Optional sidebar content -->
-      </aside>
-    </main>
-    <footer>
-      Asia Pacific College
-    </footer>
+    </div>
+
+    <div class="menu">
+      <ul>
+        <li>Plays badminton</li>
+        <li>Loves cycling</li>
+        <li>Obsessed with purple</li>
+        <li>Loves strolling</li>
+      </ul>
+    </div>
+
+    <div class="content">
+      <h1>Tricia's Introduction</h1>
+
+      <p>
+        About Me<br>
+        Name: Patricia Marie D. Valera<br>
+        Nickname: Tricia<br>
+      </p>
+
+      <p>
+        Education / Achievements<br>
+        Course: Bachelor of Science in Information Technology<br>
+      </p>
+
+      <p>
+        Hobbies & Interests<br>
+        - Listening to music<br>
+        - Watching movies, animes, series<br>
+        - Reading novels, manhwa, wattpad<br>
+      </p>
+    </div>
+
+    <div class="pic">
+      <h2>Picture Gallery</h2>
+      <ul>
+        <img
+          src="https://i.pinimg.com/originals/aa/29/e9/aa29e9cae0a07ebf8444331813e8aba0.jpg"
+          alt="kuromi"
+          width="250"
+          height="250"
+        >
+      </ul>
+    </div>
+
+    <div class="footer">
+      <p>The End.</p>
+    </div>
+
   </div>
 </template>
 
 <script></script>
+
 <style src="./css/PersonalProfile.css"></style>
